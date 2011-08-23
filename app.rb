2e36e6ffb3309f08068eff233bd99348308febbe
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-23 06:52:36 -0700

    Create Task table and call ::auto_migrate

--- app.rb
+++ app.rb
@@ -5,6 +5,17 @@ require 'data_mapper'
 DataMapper.setup(:default,
               "sqlite://#{File.expand_path(File.dirname(__FILE__))}/db/tdrb.db")
 
+class Task
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :body,       Text
+  property :created_at, DateTime
+  property :tags,       CommaSeparatedList
+end
+
+DataMapper.auto_migrate!
+
 get '/' do
   'Hello, world.'
 end
