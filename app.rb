b27a68565534207b333bfcd651e267191bc9f8c6
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-23 06:51:43 -0700

    Set up connection to database

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'slim'
 require 'data_mapper'
 
+DataMapper.setup(:default,
+              "sqlite://#{File.expand_path(File.dirname(__FILE__))}/db/tdrb.db")
+
 get '/' do
   'Hello, world.'
 end
